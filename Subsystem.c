d11947e95c865e1f35e345cebd464f6a407b7b83
Prabodh38 <prabodhjagtap5703@gmail.com>
2024-08-10 22:13:38 +0530

    Implementation of relay block using basic blocks avaliable in Simulink and code for relay block.

---
+++ Subsystem.c
@@ -0,0 +1,95 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: Subsystem.c
+ *
+ * Code generated for Simulink model 'Subsystem'.
+ *
+ * Model version                  : 1.3
+ * Simulink Coder version         : 24.1 (R2024a) 19-Nov-2023
+ * C/C++ source code generated on : Sat Aug 10 21:50:43 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "Subsystem.h"
+
+/* Block states (default storage) */
+DW_Subsystem_T Subsystem_DW;
+
+/* External inputs (root inport signals with default storage) */
+ExtU_Subsystem_T Subsystem_U;
+
+/* External outputs (root outports fed by signals with default storage) */
+ExtY_Subsystem_T Subsystem_Y;
+
+/* Real-time model */
+static RT_MODEL_Subsystem_T Subsystem_M_;
+RT_MODEL_Subsystem_T *const Subsystem_M = &Subsystem_M_;
+
+/* Model output function */
+void Subsystem_output(void)
+{
+  /* Outputs for Atomic SubSystem: '<Root>/Subsystem' */
+  /* Switch: '<S1>/Switch' incorporates:
+   *  Inport: '<Root>/Inport'
+   *  Switch: '<S1>/Switch1'
+   */
+  if (Subsystem_U.Inport >= 5.0) {
+    /* Outport: '<Root>/Out1' incorporates:
+     *  Constant: '<S1>/Constant'
+     */
+    Subsystem_Y.Out1 = 1.0;
+  } else if (Subsystem_U.Inport > -5.0) {
+    /* Switch: '<S1>/Switch1' incorporates:
+     *  Delay: '<S1>/Delay'
+     *  Outport: '<Root>/Out1'
+     */
+    Subsystem_Y.Out1 = Subsystem_DW.Delay_DSTATE[0];
+  } else {
+    /* Outport: '<Root>/Out1' incorporates:
+     *  Constant: '<S1>/Constant2'
+     *  Switch: '<S1>/Switch1'
+     */
+    Subsystem_Y.Out1 = 0.0;
+  }
+
+  /* End of Switch: '<S1>/Switch' */
+  /* End of Outputs for SubSystem: '<Root>/Subsystem' */
+}
+
+/* Model update function */
+void Subsystem_update(void)
+{
+  /* Update for Atomic SubSystem: '<Root>/Subsystem' */
+  /* Update for Delay: '<S1>/Delay' incorporates:
+   *  Outport: '<Root>/Out1'
+   */
+  Subsystem_DW.Delay_DSTATE[0] = Subsystem_DW.Delay_DSTATE[1];
+  Subsystem_DW.Delay_DSTATE[1] = Subsystem_Y.Out1;
+
+  /* End of Update for SubSystem: '<Root>/Subsystem' */
+}
+
+/* Model initialize function */
+void Subsystem_initialize(void)
+{
+  /* (no initialization code required) */
+}
+
+/* Model terminate function */
+void Subsystem_terminate(void)
+{
+  /* (no terminate code required) */
+}
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
