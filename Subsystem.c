f55ae1d3be4790bb2fefaed8f07c00a611305cd0
Prabodh38 <prabodhjagtap5703@gmail.com>
2024-08-10 22:30:31 +0530

    Code generated for relay block.

--- Subsystem.c
+++ Subsystem.c
@@ -1,22 +1,3 @@
-/*
- * Academic License - for use in teaching, academic research, and meeting
- * course requirements at degree granting institutions only.  Not for
- * government, commercial, or other organizational use.
- *
- * File: Subsystem.c
- *
- * Code generated for Simulink model 'Subsystem'.
- *
- * Model version                  : 1.3
- * Simulink Coder version         : 24.1 (R2024a) 19-Nov-2023
- * C/C++ source code generated on : Sat Aug 10 21:50:43 2024
- *
- * Target selection: ert.tlc
- * Embedded hardware selection: Intel->x86-64 (Windows64)
- * Code generation objectives: Unspecified
- * Validation result: Not run
- */
-
 #include "Subsystem.h"
 
 /* Block states (default storage) */
